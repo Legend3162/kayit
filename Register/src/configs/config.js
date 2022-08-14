15202110cb690f9a59bec165b3c5073177afe1d5
Legend3162 <102464543+Legend3162@users.noreply.github.com>
2022-08-14 10:57:29 +0300

    kayit
    butonlu

---
+++ Register/src/configs/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    OwnerID: "729761039306063893", //BOT OWNER
+    Owners: ["729761039306063893"], //SUNUCU OWNER
+    GuildID: "977859733908238336", //SUNUCU ID
+    VoiceID: "1006527071276761129", //SES ODASI ID
+
+    Token: "ODc3MTg5NDk5MTMyNzMxNDAy.GVbdBA.7oWc0oC3vwA1OOP2pIeRsRbSZ81bcOGiMomi44", //BOT TOKEN
+    MongoIRL: "mongodb+srv://Legend31:Melek098@cluster0.dphxs.mongodb.net/Legend31?retryWrites=true&w=majority", //MONGO URL
+    Prefixs: ["!", "."], //PREFIXLER
+
+    Tag: "★", //SUNUCU TAGINIZ
+    SecondaryTag: "★", //DOKUNMASANIZDA OLUR
+
+    Status: "dnd", // dnd - online - idle
+    Message: "★ S A N C H E S", //BOT DURUMU
+    Footer: "LEGEND" //setFooter
+}
